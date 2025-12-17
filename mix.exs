1746852805af3909bf8caa7aa508f26c6ce5cee2
root <root@dev.zenclock.eu>
2025-12-17 15:47:10 +0000

    Prep for 1.0.5

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule BeamLabCountries.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/BeamLabEU/beamlab_countries"
-  @version "1.0.4"
+  @version "1.0.5"
 
   def project do
     [
